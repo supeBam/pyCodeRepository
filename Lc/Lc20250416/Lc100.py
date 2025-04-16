44a9a739a292189bfd9aa79737cbfefedb8f362a
weiwenchao <846032492@qq.com>
2025-04-16 17:59:45 +0800

    feat: 深度dfs

---
+++ Lc/Lc20250416/Lc100.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        elif p.val != q.val:
+            return False
+        else:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
