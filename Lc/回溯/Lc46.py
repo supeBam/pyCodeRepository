7fe96bd9eb251fa5ea2467c98b8b30bd847e74d3
weiwenchao <846032492@qq.com>
2025-04-18 15:12:09 +0800

    feat: 回溯

--- Lc/回溯/Lc46.py
+++ Lc/回溯/Lc46.py
@@ -1,18 +1,15 @@
-from sys import flags
 from typing import List
 
 
 class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+    def permute(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         ans = []
         flag = [False] * n
         path = []
-        d = set()
 
-        def dfs(deep, path, flag) -> None:
-            if n == len(path) and (frozenset(path) not in d):
-                d.add(frozenset(path))
+        def dfs(deep: int, path: List[int], flag: List[bool]) -> None:
+            if n == len(path):
                 ans.append(path.copy())
                 return
             for i, v in enumerate(nums):
@@ -31,7 +28,6 @@ class Solution:
         dfs(0, path, flag)
         return ans
 
-
 main = Solution()
-ans = main.permuteUnique([1, 1, 2])
+ans = main.permute([1, 2, 3])
 print(ans)
