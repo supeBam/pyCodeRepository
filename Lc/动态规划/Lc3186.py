ebc0f8f3010b669d1a8ea93b2ac597a3398e500e
weiwenchao <846032492@qq.com>
2025-05-06 18:12:23 +0800

    feat: 动态规划 (值域打家劫舍)

---
+++ Lc/动态规划/Lc3186.py
@@ -0,0 +1,21 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def maximumTotalDamage(self, power: List[int]) -> int:
+        cnt = Counter(power)
+        sorted_cnt = sorted(cnt.keys())
+        dp = [0] * (len(sorted_cnt) + 1)
+
+        j = 0
+        for i, v in enumerate(sorted_cnt):
+            while sorted_cnt[j] < v - 2:
+                j += 1
+            dp[i + 1] = max(dp[i], dp[j] + cnt[v] * v)
+        return dp[-1]
+
+if __name__ == '__main__':
+    s = Solution()
+    # print(s.maximumTotalDamage([1, 1, 3, 4]))
+    print(s.maximumTotalDamage([1, 1, 1, 1, 1, 1]))
