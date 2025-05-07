5078fd91ea11981b7dde56e04fe857ecd87b8ec5
weiwenchao <846032492@qq.com>
2025-05-07 18:18:26 +0800

    feat: 动态规划(53题最大子数组和变形)

--- Lc/动态规划/Lc2606.py
+++ Lc/动态规划/Lc2606.py
@@ -3,17 +3,18 @@ from typing import List
 
 class Solution:
     def maximumCostSubstring(self, s: str, chars: str, vals: List[int]) -> int:
+        n = len(s)
         d = [i for i in range(1, 27)]
         for i, v in enumerate(chars):
             d[ord(v) - 97] = vals[i]
 
-        nums = [0] * (len(s))
+        nums = [0] * n
         for i, v in enumerate(s):
             nums[i] = d[ord(v) - 97]
 
-        dp = [0] * (len(s))
+        dp = [0] * n
         dp[0] = nums[0]
-        for i in range(1, len(s)):
+        for i in range(1, n):
             if dp[i - 1] > 0:
                 dp[i] = nums[i] + dp[i - 1]
             else:
