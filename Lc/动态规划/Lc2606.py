6a50c2f36f84db8324f40c416280f931b82752e8
weiwenchao <846032492@qq.com>
2025-05-08 10:28:18 +0800

    feat: 动态规划(53题最大子数组和变形) 优化

--- Lc/动态规划/Lc2606.py
+++ Lc/动态规划/Lc2606.py
@@ -22,6 +22,22 @@ class Solution:
         return max(dp) if max(dp) > 0 else 0
 
 
+    # 优化1： 简化
+    def maximumCostSubstring2(self, s: str, chars: str, vals: List[int]) -> int:
+        n = len(s)
+        # zip() 按位置 一 一 对应, 长度不同时按最小长度来
+        d = dict(zip(chars, vals))
+        nums = [d.get(c, ord(c) - 96) for c in s]
+
+        dp = [0] * n
+        dp[0] = nums[0]
+        for i in range(1, n):
+            if dp[i - 1] > 0:
+                dp[i] = nums[i] + dp[i - 1]
+            else:
+                dp[i] = nums[i]
+        return max(dp) if max(dp) > 0 else 0
+
 if __name__ == '__main__':
     s = Solution()
     # print(s.maximumCostSubstring(s="adaa", chars="d", vals=[-1000]))
